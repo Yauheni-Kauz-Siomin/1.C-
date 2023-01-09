a054ed44015c76a8e4c1a2ead220a98a80268b96
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-01-09 14:11:43 +0300

    add Example005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // оператор перевода всех символов строки в нижний регистр
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
